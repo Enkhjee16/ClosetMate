aba528a55723599c2e8b65d9faa4e8c72a345245
BDCRL0 <lillaszabod.04@gmail.com>
2026-04-30 12:27:44 +0200

    Add calendar planner and analytics screen

--- app/src/main/java/nje/hu/closetmate/MainActivity.java
+++ app/src/main/java/nje/hu/closetmate/MainActivity.java
@@ -1,33 +1,53 @@
 package nje.hu.closetmate;
 
 import android.content.Intent;
+
 import android.os.Bundle;
+
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnAddClothing, btnWardrobe;
+    private Button btnAddClothing, btnWardrobe, btnCalendar, btnAnalytics;
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_main);
 
         btnWardrobe = findViewById(R.id.btnWardrobe);
+
         btnAddClothing = findViewById(R.id.btnAddClothing);
 
-        btnWardrobe.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, WardrobeActivity.class);
-            startActivity(intent);
-        });
+        btnCalendar = findViewById(R.id.btnCalendar);
 
-        btnAddClothing.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, AddClothingActivity.class);
-            startActivity(intent);
-        });
+        btnAnalytics = findViewById(R.id.btnAnalytics);
+
+        btnWardrobe.setOnClickListener(v ->
+
+                startActivity(new Intent(MainActivity.this, WardrobeActivity.class)));
+
+        btnAddClothing.setOnClickListener(v ->
+
+                startActivity(new Intent(MainActivity.this, AddClothingActivity.class)));
+
+        btnCalendar.setOnClickListener(v ->
+
+                startActivity(new Intent(MainActivity.this, CalendarActivity.class)));
+
+        btnAnalytics.setOnClickListener(v ->
+
+                startActivity(new Intent(MainActivity.this, AnalyticsActivity.class)));
+
     }
+
 }
